a2a5d91ee0674796ac94b333e690c27b5ce86717
jobsyu <1175585627@qq.com>
2015-07-21 15:33:56 +0800

    Initial Commit

---
+++ TVPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TVPlay
+//
+//  Created by jobs on 15/7/21.
+//  Copyright (c) 2015年 jobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TVPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TVPlay
+//
+//  Created by jobs on 15/7/21.
+//  Copyright (c) 2015年 jobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
